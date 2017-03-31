18e3aaed02323f6635ffe628d6c14a2dded207df
Abytecoder <abytecoder@gmail.com>
2017-03-31 16:30:40 +0530

    Added support for decimal,fractions and alphabetic series to WrapperClass

--- modules/WrapperClass.py
+++ modules/WrapperClass.py
@@ -10,12 +10,14 @@ def format_solution(sol_dict):
         sys.exit(0)
     if sol_dict == -1:
         print("Invalid Question")
+        sys.exit(0)
     if sol_dict == -2:
         print("The above series is not yet supported")
-    print(sol_dict['result'])
+        sys.exit(0)
+    print(str(sol_dict['result']))
     steps = sol_dict['steps']
     for i in range(len(steps)):
-        print("Step"+str(i+1)+" : "+steps[i])
+        print("Step"+str(i+1)+" : "+str(steps[i]))
 
 #analyses the question and calls the solution module
 def questionAnalyser(ques):
@@ -24,19 +26,20 @@ def questionAnalyser(ques):
     if re.search(r" sum ",ques,re.I|re.M):
         tp = 1
     #extract the part which contains series 1,2,3,_,5,.....
-    matchObj = re.search(r"(((\d+|_|\-|\d+\/\d+)\,)+)",ques,re.I|re.M)
+    matchObj = re.search(r"(((\-?\d+|_|\-|\-?\d+\/\d+|\-?\d+\.\d+|[a-zA-Z])\,)+)",ques,re.I|re.M)
     if matchObj:
         #series found
         series = matchObj.group(1);
         series_list = series.split(',')
         series_list = series_list[:len(series_list)-1]
         for i in range(len(series_list)):
-            if series_list[i] == '-' or series_list[i] == '_' or isinstance(eval(series_list[i]),str):
+            if series_list[i] == '-' or series_list[i] == '_':
                 #position of - or _ if present
                 #since last item contains ...
                  findPos = i
             else:
-                series_list[i] = eval(series_list[i])
+                if not(series_list[i].isalpha()):
+                    series_list[i] = eval(series_list[i])
         if ( findPos != -1 ) :
             #if missing number is present in series
             odict = s.solveSeries(series_list,findPos,tp)
@@ -54,8 +57,8 @@ def questionAnalyser(ques):
     else:
         return -2;
 def main():
-    print("Aptitude Engine v0.2 alpha")
-    print("Works for AP,GP and HP series problems")
+    print("Aptitude Engine v0.5 beta")
+    print("Works for AP,GP,HP, Diffrence in AP,GP,HP and even odd relations")
     ques = input("Question:")
     sol_dict = questionAnalyser(ques)
     format_solution(sol_dict)
